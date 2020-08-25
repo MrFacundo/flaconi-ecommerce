b026f41a247b4340e3c3713f7a6adcd82fe465f8
Facundo Troitero <ftroitero@gmail.com>
2020-08-26 01:02:19 +0200

    created graphQL server

---
+++ schema.js
@@ -0,0 +1,47 @@
+const axios = require("axios");
+
+const {
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLList,
+  GraphQLSchema,
+} = require("graphql");
+
+// Product Type
+
+const ProductType = new GraphQLObjectType({
+  name: "Product",
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    slug: { type: GraphQLString },
+    brand: { type: GraphQLString },
+    type: { type: GraphQLString },
+    image: { type: GraphQLString },
+    price: { type: GraphQLInt },
+    size: { type: GraphQLString },
+    rating: { type: GraphQLInt },
+  }),
+});
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    products: {
+      type: new GraphQLList(ProductType),
+      resolve(parent, args) {
+        return axios
+          .get(
+            "https://raw.githubusercontent.com/Flaconi/coding-challenges/master/frontend-engineer/resources/productlist.json"
+          )
+          .then((res) => res.data);
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
